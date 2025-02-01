1b7b4fac380b55e7373b6f8d45d87b21b1881ff1
DanielS6327 <daniel2007cs@gmail.com>
2025-02-01 22:05:38 +0000

    adicionei a abilidade de escolher a pasta onde o ficheiro é criado
    adicionei  uma parte ao codigo que permite mudar a pasta onde guardamos os valores, se quiserem experimentar isto vejam os WARNING e ATTENTION. tirei o import time pq n estava a ser utilizado

--- saves_arduino_values_in_csv.py
+++ saves_arduino_values_in_csv.py
@@ -1,15 +1,28 @@
 import serial
 import csv
-import time
+import os
 
-# Set the correct port where your Arduino is connected
-port = 'COM3'  # Update with your Arduino's port
+
+# IMPORTANT: Set the correct port where your Arduino is connected
+port = 'COM3'  # Update with your Arduino's port, it's easy to check on the arduino IDE
 baudrate = 9600
 ser = serial.Serial(port, baudrate)
 
-# Open a CSV or TXT file to save the data
-file_name = "sensor_data.csv"  # You can change this to .txt if you want
-with open(file_name, 'w', newline='') as file:
+
+folder_path = r"C:\\Users\Aluno\Documents\\cansat\\" # IMPORTANT: change this to the folder path where you want the file to go
+
+os.makedirs(folder_path, exist_ok=True)  # I think it's suposed to check if the folder exists and create it if it doesn't
+
+file_name = "sensor_data.csv" # Change this if you want a diferent name for the file created or a diferent type for exemple .txt
+
+file_path = os.path.join(folder_path, file_name)
+
+
+#WARNING: if there is already a "file_name" file this will overwrite it,
+# if you got useful data don't forget to rename it before using this program again 
+
+# Open a csv or txt file to save the data
+with open(file_path, 'w', newline='') as file:
     writer = csv.writer(file)
     
     # Write header if CSV
