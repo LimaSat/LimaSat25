154f882df376418e738aa40427eb7b43ebaf7f6b
DanielS6327 <daniel2007cs@gmail.com>
2025-03-07 17:17:57 +0000

    transmiter

---
+++ APC_Transmitter_simples/APC_Transmitter_simples.ino
@@ -0,0 +1,20 @@
+#include <Wire.h>
+
+
+void setup() {
+    Serial.begin(9600);  // Serial communication with APC220
+    delay(2000);  // Delay to give the APC220 time to initialize
+    
+  // Check if the APC220 responded successfully (optional)
+  delay(1000);
+    Wire.begin();
+
+}
+
+void loop() {
+    
+    float sensorUV = (float)analogRead(A1) * 5000 / 1023.0;
+    Serial.print(sensorUV); Serial.print(",");
+    Serial.println("Sending data...");
+    delay(1000); // Send data every second
+}
